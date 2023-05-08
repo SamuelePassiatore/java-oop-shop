1ddf36ac9b21d28b23685c718e455a4ba1e07a25
SamuelePassiatore <semmisno2@protonmail.com>
2023-05-08 17:53:11 +0200

    changes

--- src/jana60/bank/Conto.java
+++ src/jana60/bank/Conto.java
@@ -52,7 +52,6 @@ public class Conto {
     }
     
     public String formattaSaldo(double saldo) {
-    	DecimalFormat df = new DecimalFormat("#,###.##");
-    	return df.format(saldo) + "€";
+    	return saldo + "€";
     }
 }
